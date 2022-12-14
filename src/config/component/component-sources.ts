0732c8106531b1d45c91a6aff026937a6b626a87
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2022-12-14 00:32:13 +0000

    update component sources

--- src/config/component/component-sources.ts
+++ src/config/component/component-sources.ts
@@ -1,14 +1,23 @@
-import { IS_CHEWY_CONTRIBUTOR } from "../chewy/constants";
+import { IS_CHEWY_CONTRIBUTOR } from "../global/constants";
 
 interface ChewyComponents {
     hasura: string;
     nextjs: string;
+    nestjs: string;
+    'ory-kratos': string;
+    'ory-oathkeeper': string;
 }
 
 export const components: ChewyComponents = IS_CHEWY_CONTRIBUTOR ? {
     'hasura': '/workspace/chewy-global/components/hasura',
     'nextjs': '/workspace/chewy-global/components/nextjs',
+    'nestjs': '/workspace/chewy-global/components/nestjs',
+    'ory-kratos': '/workspace/chewy-global/components/ory-kratos',
+    'ory-oathkeeper': '/workspace/chewy-global/components/ory-oathkeeper',
 } : {
     'hasura': 'https://github.com/gochewy/hasura.git',
     'nextjs': 'https://github.com/gochewy/nextjs.git',
+    'nestjs': 'https://github.com/gochewy/nestjs.git',
+    'ory-kratos': '/workspace/chewy-global/components/ory-kratos',
+    'ory-oathkeeper': '/workspace/chewy-global/components/ory-oathkeeper',
 };
