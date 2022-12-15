a24937afe3c911c6faf200798dc48a681336a684
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2022-12-15 02:25:55 +0000

    updated resource name error message

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,5 @@ module.exports = {
     "react-app",
     "prettier/@typescript-eslint",
     "plugin:prettier/recommended"
-  ],
-  "settings": {
-    "react": {
-      "version": "999.999.999"
-    }
-  }
+  ]
 }
--- src/utils/resource-name-schema.ts
+++ src/utils/resource-name-schema.ts
@@ -1,4 +1,9 @@
 import { z } from 'zod';
 
-const resourceNameSchema = z.string().regex(/^[a-z0-9-]+$/);
+const resourceNameSchema = z
+  .string()
+  .regex(
+    /^[a-z0-9-]+$/,
+    'Resource name must be lowercase, alphanumeric, may contain hyphens, and must start with a letter.'
+  );
 export default resourceNameSchema;
