f628bf6c30439545b986f031d52c5558ca077f9f
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2022-12-17 21:22:42 +0000

    fix resource name schema regex

--- src/utils/resource-name-schema.ts
+++ src/utils/resource-name-schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 const resourceNameSchema = z
   .string()
   .regex(
-    /^[a-z0-9-]+$/,
-    'Resource name must be lowercase, alphanumeric, may contain hyphens, and must start with a letter.'
+    /^[a-z]+[a-z\d-]*[\da-z]*(?<!-)$/,
+    'Resource name must be lowercase, alphanumeric, may contain dashes, must start with a letter, and cannot end in a dash.'
   );
 export default resourceNameSchema;
