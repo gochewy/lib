725f92d53bfd06f5fcf0e0798617f484bf7f2b85
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2022-12-18 02:30:57 +0000

    change definition file naming

--- src/config/component/component-definition.ts
+++ src/config/component/component-definition.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod';
 import componentDefinitionSchema from './component-definition-schema';
 
 type ComponentDefinition = z.TypeOf<typeof componentDefinitionSchema>;
--- src/config/environment/environment-definition.ts
+++ src/config/environment/environment-definition.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod';
 import environmentDefinitionSchema from './environment-definition-schema';
 
 type EnvironmentDefinition = z.TypeOf<typeof environmentDefinitionSchema>;
