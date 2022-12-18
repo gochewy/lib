96cfff12541a8d1ee65d26bcac2dc0bf36e19bd7
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2022-12-18 22:13:08 +0000

    export jest config

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+module.exports = {
+  transform: {
+    '.(ts|tsx)$': require.resolve('ts-jest/dist'),
+    '.(js|jsx)$': require.resolve('babel-jest'), // jest's default
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
+  testMatch: ['<rootDir>/**/*.(spec|test).{ts,tsx,js,jsx}'],
+  testURL: 'http://localhost',
+  rootDir: './src',
+  watchPlugins: [
+    require.resolve('jest-watch-typeahead/filename'),
+    require.resolve('jest-watch-typeahead/testname'),
+  ],
+};
