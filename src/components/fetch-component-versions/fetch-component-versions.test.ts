4ae9b6cd41e8d3d52eb7d62719245fad15797556
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2022-12-24 22:54:58 +0000

    switch component to test in fetch component versions

--- src/components/fetch-component-versions/fetch-component-versions.test.ts
+++ src/components/fetch-component-versions/fetch-component-versions.test.ts
@@ -3,8 +3,10 @@ import fetchComponentVersions from './fetch-component-versions';
 
 describe('fetchComponentVersions', () => {
   it('should fetch component versions', async () => {
-    const versions = await fetchComponentVersions(componentSources.hasura);
+    const versions = await fetchComponentVersions(
+      componentSources['ory-kratos']
+    );
     const refs = versions.map(({ ref }) => ref);
-    expect(refs).toContain('v0.1.1-beta.22-12-19-10-35');
+    expect(refs).toContain('0.1.1-branch');
   });
 });
