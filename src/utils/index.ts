1534a2b6a8536c20406940e16927c8718028c55c
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2023-01-15 17:48:17 +0000

    add logging

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 /* eslint-disable prettier/prettier */
 export {default as isProjectInstalling} from './is-project-installing/is-project-installing';
 export {default as resourceNameSchema} from './resource-name-schema/resource-name-schema';
+export {default as log} from './log/log';
