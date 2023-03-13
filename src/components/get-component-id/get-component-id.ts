44ab42069d4489bb6080b7f57060a4525bfa73e8
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2023-03-13 08:43:33 +0000

    switch component id to properly use component name

--- src/components/get-component-id/get-component-id.ts
+++ src/components/get-component-id/get-component-id.ts
@@ -1,9 +1,12 @@
 import { getComponentDir } from '../../files';
+import getComponentName from '../get-component-name/get-component-name';
 import getInstalledComponentDefinition from '../get-installed-component-definition/get-installed-component-definition';
 
 export default function getComponentId(
   ...params: Parameters<typeof getComponentDir>
 ) {
   const definition = getInstalledComponentDefinition(...params);
-  return `${definition.type}-${definition.name}`;
+  const componentName = getComponentName(...params);
+  if (!componentName) throw Error('Could not get component name.');
+  return `${definition.type}-${componentName}`;
 }
