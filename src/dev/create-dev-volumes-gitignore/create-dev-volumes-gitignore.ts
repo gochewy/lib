6a011cc58f5d35631a810a523d23e4f719ee98e9
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2023-03-15 06:57:28 +0000

    remove log

--- src/dev/create-dev-volumes-gitignore/create-dev-volumes-gitignore.ts
+++ src/dev/create-dev-volumes-gitignore/create-dev-volumes-gitignore.ts
@@ -5,6 +5,5 @@ import { getDevVolumesDir } from '../../files';
 export default function createDevVolumesGitignore() {
   const gitignorePath = path.resolve(getDevVolumesDir(), '.gitignore');
   const gitignoreContent = `*`;
-  console.log('@@ volumes gitignore:', gitignorePath);
   outputFileSync(gitignorePath, gitignoreContent);
 }
